9d1569f2de6d8cc8aed101403b1b7a99bfe154af
luisdelc4839 <123612605+luisdelc4839@users.noreply.github.com>
2024-03-11 15:00:05 -0600

    finished page

--- config.js
+++ config.js
@@ -1,11 +1,25 @@
 function showTooltip(element) {
     var tooltip = element.querySelector(".tooltip");
-    tooltip.style.display = "block";
-    setTimeout(function() { tooltip.style.opacity = 1; }, 15); // Delay for CSS transition
+    if (tooltip) {
+        tooltip.style.display = "block";
+        setTimeout(function() { tooltip.style.opacity = 1; }, 15);
+    }
 }
 
 function hideTooltip(element) {
     var tooltip = element.querySelector(".tooltip");
-    tooltip.style.opacity = 0;
-    setTimeout(function() { tooltip.style.display = "none"; }, 300); // Match duration of CSS transition
+    if (tooltip) {
+        tooltip.style.opacity = 0;
+        setTimeout(function() { tooltip.style.display = "none"; }, 300);
+    }
 }
+
+function copyEmail() {
+    var emailAddress = document.getElementById("emailAddress");
+    emailAddress.select();
+    emailAddress.setSelectionRange(0, 99999); /* For mobile devices */
+    document.execCommand("copy");
+
+}
+
+
